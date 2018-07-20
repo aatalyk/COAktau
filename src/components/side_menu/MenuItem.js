01dccf638a1abc514c293ec8c7fd19fc8728fcae
aatalyk <atalyk.akash@nu.edu.kz>
2018-07-20 11:21:13 -0700

    fixed bugs inside MenuItem.js

--- src/components/side_menu/MenuItem.js
+++ src/components/side_menu/MenuItem.js
@@ -2,20 +2,13 @@ import React from 'react'
 import { View, Image, Text, TouchableOpacity } from 'react-native'
 import PropTypes from 'prop-types'
 
-<<<<<<< HEAD
 import { fonts } from '../../assets'
 
-=======
->>>>>>> menu
 export const MenuItem = ({ image, title }) => (
     <TouchableOpacity>
         <View style={styles.container}>
             <View style={styles.imageContainer}>
-<<<<<<< HEAD
                 <Image source={image} style={styles.image}/>
-=======
-                <Image source={image}/>
->>>>>>> menu
             </View>
             <View style={styles.titleContainer}>
                 <Text style={styles.title}>{ title }</Text>
@@ -30,7 +23,6 @@ const styles = {
         flexDirection: 'row',
         justifyContent: 'flex-start',
         alignItems: 'center',
-<<<<<<< HEAD
         margin: 5,
         marginLeft: 15
     },
@@ -53,28 +45,5 @@ const styles = {
         textAlign: 'left',
         color: 'black',
         fontFamily: fonts.MerriweatherRegular
-=======
-        margin: 5
-    },
-    imageContainer: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'red'
-    },
-    image: {
-        width: 50,
-        height: 50
-    },
-    titleContainer: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginLeft: 10,
-        backgroundColor: 'yellow'
-    },
-    title: {
-        fontSize: 25,
-        color: 'black'
->>>>>>> menu
     }
 }
