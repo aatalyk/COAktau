4a17a8d23d0643be7ec3195755c3434d0483fef5
ARKALYK AKASH <arkalyk.akash@nu.edu.kz>
2018-07-22 14:32:09 +0200

    minor refactor in HeldDesk

--- src/components/helpdesk/HelpdeskItem.js
+++ src/components/helpdesk/HelpdeskItem.js
@@ -1,95 +1,111 @@
 import React, { Component } from 'react';
-import { View, Text, Button, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  Image,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  LayoutAnimation,
+} from 'react-native';
 import PropTypes from 'prop-types';
 
 import { images } from '../../assets';
 
 const propTypes = {
-	item: PropTypes.object
+  item: PropTypes.object,
 };
 
 class HelpdeskItem extends Component {
-	state = {
-		isSelected: false
-	};
+  state = {
+    isSelected: false,
+  };
 
-	onPress = () => this.setState({ isSelected: !this.state.isSelected });
+  componentDidUpdate(_, prevState) {
+    if (this.state.isSelected != prevState.isSelected) {
+      LayoutAnimation.easeInEaseOut();
+    }
+  }
 
-	render() {
-		return this.state.isSelected ? (
-			<TouchableOpacity onPress={this.onPress}>
-				<View style={styles.container}>
-					<View style={styles.titleContainer}>
-						<Text style={[styles.title, { fontWeight: 'bold' }]}>{this.props.item.title}</Text>
-						<View style={styles.button}>
-							<TouchableOpacity onPress={this.onPress}>
-								<Image source={images.close} style={styles.image} />
-							</TouchableOpacity>
-						</View>
-					</View>
-					<Text style={styles.detail}>{this.props.item.date}</Text>
-					<Text style={[styles.detail, { color: 'black' }]}>{this.props.item.detail}</Text>
-					<View style={styles.separator} />
-					<Text style={[styles.detail, { color: 'black', textAlign: 'center' }]}>Не нашли ответ?</Text>
-					<View style={styles.replyButton}>
-						<Button title="Написать" />
-					</View>
-				</View>
-			</TouchableOpacity>
-		) : (
-			<TouchableOpacity onPress={this.onPress}>
-				<View style={[styles.titleContainer, { margin: 10 }]}>
-					<Text style={styles.title}>{this.props.item.title}</Text>
-					<View style={styles.button}>
-						<TouchableOpacity onPress={this.onPress}>
-							<Image source={images.plus} style={styles.image} />
-						</TouchableOpacity>
-					</View>
-				</View>
-			</TouchableOpacity>
-		);
-	}
+  onPress = () => this.setState({ isSelected: !this.state.isSelected });
+
+  onWriteButtonPress = () => this.setState({ isSelected: this.state.isSelected });
+
+  renderDetails() {
+    return (
+      <View>
+        <Text style={styles.detail}>{this.props.item.date}</Text>
+        <Text style={[styles.detail, { color: 'black' }]}>{this.props.item.detail}</Text>
+        <View style={styles.separator} />
+        <Text style={[styles.detail, { color: 'black', textAlign: 'center' }]}>
+          Не нашли ответ?
+        </Text>
+        <View style={styles.replyButton}>
+          <Button title="Написать" onPress={this.onWriteButtonPress} />
+        </View>
+      </View>
+    );
+  }
+
+  render() {
+    const { isSelected } = this.state;
+    return (
+      <View style={styles.container}>
+        <TouchableWithoutFeedback onPress={this.onPress}>
+          <View style={styles.titleContainer}>
+            <Text style={[styles.title, { fontWeight: isSelected ? 'bold' : 'normal' }]}>
+              {this.props.item.title}
+            </Text>
+            <View style={styles.button}>
+              <Image source={isSelected ? images.close : images.plus} style={styles.image} />
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+        {isSelected && this.renderDetails()}
+      </View>
+    );
+  }
 }
 
 HelpdeskItem.propTypes = propTypes;
 
 const styles = StyleSheet.create({
-	container: {
-		flexDirection: 'column',
-		justifyContent: 'flex-end',
-		margin: 10
-	},
-	titleContainer: {
-		flexDirection: 'row',
-		justifyContent: 'flex-end'
-	},
-	title: {
-		flex: 1,
-		fontSize: 18
-	},
-	detail: {
-		flex: 1,
-		paddingTop: 10,
-		fontSize: 18,
-		color: 'grey',
-		paddingBottom: 10
-	},
-	button: {
-		alignItems: 'flex-start'
-	},
-	separator: {
-		paddingTop: 10,
-		borderBottomColor: 'grey',
-		borderBottomWidth: 0.5
-	},
-	replyButton: {
-		borderColor: 'orange',
-		borderWidth: 1
-	},
-	image: {
-		width: 20,
-		height: 20
-	}
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    margin: 10,
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  title: {
+    flex: 1,
+    fontSize: 18,
+  },
+  detail: {
+    flex: 1,
+    paddingTop: 10,
+    fontSize: 18,
+    color: 'grey',
+    paddingBottom: 10,
+  },
+  button: {
+    alignItems: 'flex-start',
+  },
+  separator: {
+    paddingTop: 10,
+    borderBottomColor: 'grey',
+    borderBottomWidth: 0.5,
+  },
+  replyButton: {
+    borderColor: 'orange',
+    borderWidth: 1,
+  },
+  image: {
+    width: 20,
+    height: 20,
+  },
 });
 
 export { HelpdeskItem };
--- src/scenes/Helpdesk.js
+++ src/scenes/Helpdesk.js
@@ -5,38 +5,42 @@ import { SearchBar } from '../components/SearchBar';
 import { HelpdeskItem } from '../components/helpdesk/HelpdeskItem';
 
 const mockData = [
-	{
-		title: 'PropTypes exports a range of validators that can be used to make sure the data you receive is valid. ',
-		date: 'Updated today',
-		detail:
-			'In the following example, the nested title and body text will inherit the fontFamily from styles.baseText, but the title provides its own additional styles. The title and body will stack on top of each other on account of the literal newlines:'
-	},
-	{
-		title: 'PropTypes exports a range of validators that can be used to make sure the data you receive is valid. ',
-		date: 'Updated today',
-		detail:
-			'In the following example, the nested title and body text will inherit the fontFamily from styles.baseText, but the title provides its own additional styles. The title and body will stack on top of each other on account of the literal newlines:'
-	}
+  {
+    title:
+      'PropTypes exports a range of validators that can be used to make sure the data you receive is valid. ',
+    date: 'Updated today',
+    detail:
+      'In the following example, the nested title and body text will inherit the fontFamily from styles.baseText, but the title provides its own additional styles. The title and body will stack on top of each other on account of the literal newlines:',
+  },
+  {
+    title:
+      'PropTypes exports a range of validators that can be used to make sure the data you receive is valid. ',
+    date: 'Updated today',
+    detail:
+      'In the following example, the nested title and body text will inherit the fontFamily from styles.baseText, but the title provides its own additional styles. The title and body will stack on top of each other on account of the literal newlines:',
+  },
 ];
 
 class Helpdesk extends Component {
-	renderItem = ({ item }) => <HelpdeskItem item={item} />;
+  renderItem = ({ item }) => <HelpdeskItem item={item} />;
 
-	render() {
-		return (
-			<View style={styles.container}>
-				<SearchBar />
-				<FlatList data={mockData} renderItem={this.renderItem} />
-			</View>
-		);
-	}
+  keyExtractor = (item, index) => item.title + index;
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <SearchBar />
+        <FlatList data={mockData} renderItem={this.renderItem} keyExtractor={this.keyExtractor} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flexDirection: 'column',
-		justifyContent: 'space-around'
-	}
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
 });
 
 export { Helpdesk };
