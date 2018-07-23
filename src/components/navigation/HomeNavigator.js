be38cfc4dfdad08fa6dc8beea81a2f0498bd60c0
aatalyk <atalyk.akash@nu.edu.kz>
2018-07-23 05:13:46 -0700

    Added navigation FAQ

--- src/components/navigation/HomeNavigator.js
+++ src/components/navigation/HomeNavigator.js
@@ -2,25 +2,28 @@ import { createStackNavigator } from 'react-navigation';
 
 import { Home } from '../../scenes/Home';
 import { ServicesList } from '../services';
+import { FAQ } from '../faq/FAQ';
 
 export const HomeRoutes = {
-  Home: 'Home',
-  Services: 'Services',
+	Home: 'Home',
+	Services: 'Services',
+	FAQ: 'Faq'
 };
 
 export const HomeNavigator = createStackNavigator(
-  {
-    [HomeRoutes.Home]: Home,
-    [HomeRoutes.Services]: ServicesList,
-  },
-  {
-    headerStyle: {
-      backgroundColor: '#f4511e',
-    },
-    headerTintColor: '#fff',
-    headerTitleStyle: {
-      fontWeight: 'bold',
-    },
-    headerMode: 'screen',
-  },
+	{
+		[HomeRoutes.Home]: Home,
+		[HomeRoutes.Services]: ServicesList,
+		[HomeRoutes.FAQ]: FAQ
+	},
+	{
+		headerStyle: {
+			backgroundColor: '#f4511e'
+		},
+		headerTintColor: '#fff',
+		headerTitleStyle: {
+			fontWeight: 'bold'
+		},
+		headerMode: 'screen'
+	}
 );
