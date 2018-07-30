ed9eb5e3196b90f71ca623de1e88299715a60f84
aatalyk <atalyk.akash@nu.edu.kz>
2018-07-30 08:34:30 -0700

    Modified NewsItem

--- src/assets/textStyles.js
+++ src/assets/textStyles.js
@@ -1,12 +1,15 @@
 import { StyleSheet } from 'react-native';
+import { fonts } from '../assets';
 
 export const textStyles = StyleSheet.create({
-  p: {
-    fontSize: 16,
-    color: 'black',
-  },
-  h1: {
-    fontSize: 18,
-    color: 'black',
-  },
+	p: {
+		fontSize: 16,
+		fontFamily: fonts.MerriweatherRegular,
+		color: 'black'
+	},
+	h1: {
+		fontSize: 18,
+		fontFamily: fonts.MerriweatherBlack,
+		color: 'black'
+	}
 });
