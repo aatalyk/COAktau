57b118951e332ccac10d1834a7ea7c4c605680c1
Arkalyk Akash <arkalyk.akash@nu.edu.kz>
2018-08-24 18:10:21 +0200

    Header titles localized

--- src/components/navigation/Header.js
+++ src/components/navigation/Header.js
@@ -1,58 +1,67 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import PropTypes from 'prop-types';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
-import { colors, textStyles } from '../../assets';
+import { colors, textStyles } from "../../assets";
 
 const propTypes = {
   leftItem: PropTypes.node,
   rightItem: PropTypes.node,
-  title: PropTypes.node,
+  titleKaz: PropTypes.string,
+  titleRus: PropTypes.string,
+  lang: PropTypes.oneOf(["kaz", "rus"])
 };
 
-export const Header = ({ leftItem, rightItem, title }) => (
+const HeaderComponent = ({ leftItem, rightItem, titleKaz, titleRus, lang }) => (
   <View>
     <View style={styles.orangeView} />
     <View style={styles.container}>
       <View style={styles.leftItem}>{leftItem}</View>
-      <Text style={textStyles.p}>{title}</Text>
+      <Text style={textStyles.p}>{lang === "kaz" ? titleKaz : titleRus}</Text>
       <View style={styles.rightItem}>{rightItem}</View>
     </View>
   </View>
 );
 
-Header.propTypes = propTypes;
+HeaderComponent.propTypes = propTypes;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'white',
-    justifyContent: 'space-between',
-    flexDirection: 'row',
+    backgroundColor: "white",
+    justifyContent: "space-between",
+    flexDirection: "row",
     height: 56,
-    alignItems: 'center',
+    alignItems: "center",
     shadowOffset: {
       width: 0,
-      height: 3,
+      height: 3
     },
     shadowOpacity: 0.3,
     shadowRadius: 2,
     elevation: 3,
-    shadowColor: 'black',
+    shadowColor: "black"
   },
   leftItem: {
     marginLeft: 10,
-    alignItems: 'flex-start',
+    alignItems: "flex-start",
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center"
   },
   rightItem: {
-    alignItems: 'flex-end',
+    alignItems: "flex-end",
     flex: 1,
-    justifyContent: 'center',
-    marginRight: 10,
+    justifyContent: "center",
+    marginRight: 10
   },
   orangeView: {
     backgroundColor: colors.orange,
-    height: 20,
-  },
+    height: 20
+  }
 });
+
+const mapStateToProps = ({ settings }) => ({
+  lang: settings.lang
+});
+
+export const Header = connect(mapStateToProps)(HeaderComponent);
