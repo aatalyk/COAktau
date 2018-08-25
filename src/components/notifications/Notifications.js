ae0bc570e23fefd034bee08da6c676a9fcc0b64a
Arkalyk Akash <arkalyk.akash@nu.edu.kz>
2018-08-25 16:27:01 +0200

    notification navigation bug fixed

--- src/components/notifications/Notifications.js
+++ src/components/notifications/Notifications.js
@@ -1,71 +1,77 @@
-import React, { Component } from 'react';
-import { View, StyleSheet, FlatList } from 'react-native';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import { View, StyleSheet, FlatList } from "react-native";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-import { NotificationItem } from './NotificationItem';
-import { colors } from '../../assets';
-import { fetchNotifsRequested } from '../../actions';
+import { NotificationItem } from "./NotificationItem";
+import { colors } from "../../assets";
+import { fetchNotifsRequested } from "../../actions";
 
 const propTypes = {
-	navigation: PropTypes.object,
-	notifsItems: PropTypes.array,
-	fetchNotifsRequested: PropTypes.func,
-	lang: PropTypes.string
+  navigation: PropTypes.object,
+  notifsItems: PropTypes.array,
+  fetchNotifsRequested: PropTypes.func,
+  lang: PropTypes.string
 };
 
 class NotificationsScreen extends Component {
-	componentDidMount() {
-		this.props.fetchNotifsRequested();
-	}
+  componentDidMount() {
+    this.props.fetchNotifsRequested();
+  }
 
-	renderItem = ({ item }) => {
-		const localizedItem = item[this.props.lang];
-		return <NotificationItem item={localizedItem} onPress={() => this.onPress(localizedItem)} />;
-	};
+  renderItem = ({ item }) => {
+    const localizedItem = item[this.props.lang];
+    return (
+      <NotificationItem
+        item={localizedItem}
+        onPress={() => this.onPress(localizedItem)}
+      />
+    );
+  };
 
-	renderSeparator = () => <View style={styles.separator} />;
+  renderSeparator = () => <View style={styles.separator} />;
 
-	onPress = item => this.props.navigation.navigate('NotificationPage', { item });
+  onPress = item =>
+    this.props.navigation.navigate("NotificationPage", { item });
 
-	render() {
-		return (
-			<View style={styles.container}>
-				<FlatList
-					data={this.props.notifsItems}
-					renderItem={this.renderItem}
-					style={styles.flatList}
-					ItemSeparatorComponent={this.renderSeparator}
-					keyExtractor={(_, index) => index + ''}
-				/>
-			</View>
-		);
-	}
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.props.notifsItems}
+          renderItem={this.renderItem}
+          style={styles.flatList}
+          ItemSeparatorComponent={this.renderSeparator}
+          keyExtractor={(_, index) => index + ""}
+        />
+      </View>
+    );
+  }
 }
 
 NotificationsScreen.propTypes = propTypes;
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: colors.blueUltraLight
-	},
-	flatList: {
-		flex: 1,
-		marginVertical: 20
-	},
-	separator: {
-		backgroundColor: colors.grayUltraLight,
-		height: 1
-	}
+  container: {
+    flex: 1,
+    backgroundColor: colors.blueUltraLight
+  },
+  flatList: {
+    flex: 1,
+    marginVertical: 20
+  },
+  separator: {
+    backgroundColor: colors.grayUltraLight,
+    height: 1
+  }
 });
 
 const mapStateToProps = ({ notifs, settings }) => ({
-	notifsItems: notifs.notifsItems,
-	lang: settings.lang
+  notifsItems: notifs.notifsItems,
+  lang: settings.lang
 });
 
 export const Notifications = connect(
-	mapStateToProps,
-	{ fetchNotifsRequested }
+  mapStateToProps,
+  { fetchNotifsRequested }
 )(NotificationsScreen);
--- src/scenes/Home.js
+++ src/scenes/Home.js
@@ -1,105 +1,108 @@
-import React, { Component } from 'react';
-import { View, Dimensions, StyleSheet, Platform } from 'react-native';
-import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { View, Dimensions, StyleSheet, Platform } from "react-native";
+import { TabView, TabBar, SceneMap } from "react-native-tab-view";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
-import { ServicesAndFAQ } from '../components/services';
-import { AutoPagingFlatList } from '../components/home/AutoPagingFlatList';
-import { colors } from '../assets';
-import { fetchNewsRequested } from '../actions';
-import { Notifications } from '../components/notifications';
+import { ServicesAndFAQ } from "../components/services";
+import { AutoPagingFlatList } from "../components/home/AutoPagingFlatList";
+import { colors } from "../assets";
+import { fetchNewsRequested } from "../actions";
+import { Notifications } from "../components/notifications";
 
-import { CalcScreen } from '../components/calculator';
+import { CalcScreen } from "../components/calculator";
 
-const SCREEN_WIDTH = Dimensions.get('window').width;
+const SCREEN_WIDTH = Dimensions.get("window").width;
 
 const propTypes = {
-	navigation: PropTypes.object,
-	newsItems: PropTypes.array,
-	fetchNewsRequested: PropTypes.func,
-	lang: PropTypes.string
+  navigation: PropTypes.object,
+  newsItems: PropTypes.array,
+  fetchNewsRequested: PropTypes.func,
+  lang: PropTypes.string
 };
 
 class HomeScreen extends Component {
-	state = {
-		index: 0,
-		routes: [{ key: 'first', title: 'First' }, { key: 'second', title: 'Second' }]
-	};
+  state = {
+    index: 0,
+    routes: [
+      { key: "first", title: "First" },
+      { key: "second", title: "Second" }
+    ]
+  };
 
-	constructor(props) {
-		super(props);
-		this.props.fetchNewsRequested();
-	}
+  constructor(props) {
+    super(props);
+    this.props.fetchNewsRequested();
+  }
 
-	renderTabBar = props => (
-		<TabBar
-			{...props}
-			style={styles.tabbar}
-			labelStyle={styles.tabbarLabel}
-			indicatorStyle={styles.tabbarIndicator}
-		/>
-	);
+  renderTabBar = props => (
+    <TabBar
+      {...props}
+      style={styles.tabbar}
+      labelStyle={styles.tabbarLabel}
+      indicatorStyle={styles.tabbarIndicator}
+    />
+  );
 
-	onTabViewIndexChange = index => this.setState({ index });
+  onTabViewIndexChange = index => this.setState({ index });
 
-	render() {
-		return (
-			<View style={styles.container}>
-				<AutoPagingFlatList
-					data={this.props.newsItems}
-					lang={this.props.lang}
-					navigation={this.props.navigation}
-				/>
-				<TabView
-					renderTabBar={this.renderTabBar}
-					navigationState={this.state}
-					renderScene={SceneMap({
-						first: () => <Notifications />,
-						second: () => <ServicesAndFAQ navigation={this.props.navigation} />
-					})}
-					onIndexChange={this.onTabViewIndexChange}
-					initialLayout={styles.tabViewInitialLayout}
-					style={{ marginTop: Platform.OS === 'ios' ? 0 : -5 }}
-					useNativeDriver
-				/>
-			</View>
-		);
-	}
+  render() {
+    return (
+      <View style={styles.container}>
+        <AutoPagingFlatList
+          data={this.props.newsItems}
+          lang={this.props.lang}
+          navigation={this.props.navigation}
+        />
+        <TabView
+          renderTabBar={this.renderTabBar}
+          navigationState={this.state}
+          renderScene={SceneMap({
+            first: () => <Notifications navigation={this.props.navigation} />,
+            second: () => <ServicesAndFAQ navigation={this.props.navigation} />
+          })}
+          onIndexChange={this.onTabViewIndexChange}
+          initialLayout={styles.tabViewInitialLayout}
+          style={{ marginTop: Platform.OS === "ios" ? 0 : -5 }}
+          useNativeDriver
+        />
+      </View>
+    );
+  }
 }
 
 HomeScreen.propTypes = propTypes;
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: 'white'
-	},
-	tabbar: {
-		backgroundColor: Platform.OS === 'android' ? 'white' : colors.transparent,
-		height: 44,
-		borderBottomColor: colors.grayUltraLight,
-		borderBottomWidth: 1
-	},
-	tabbarLabel: {
-		color: 'black'
-	},
-	tabbarIndicator: {
-		backgroundColor: colors.blue,
-		height: 3
-	},
-	tabViewInitialLayout: {
-		width: SCREEN_WIDTH,
-		height: 44
-	}
+  container: {
+    flex: 1,
+    backgroundColor: "white"
+  },
+  tabbar: {
+    backgroundColor: Platform.OS === "android" ? "white" : colors.transparent,
+    height: 44,
+    borderBottomColor: colors.grayUltraLight,
+    borderBottomWidth: 1
+  },
+  tabbarLabel: {
+    color: "black"
+  },
+  tabbarIndicator: {
+    backgroundColor: colors.blue,
+    height: 3
+  },
+  tabViewInitialLayout: {
+    width: SCREEN_WIDTH,
+    height: 44
+  }
 });
 
 const mapStateToProps = ({ news, settings }) => ({
-	newsItems: news.newsItems,
-	lang: settings.lang
+  newsItems: news.newsItems,
+  lang: settings.lang
 });
 
 export const Home = connect(
-	mapStateToProps,
-	{ fetchNewsRequested }
+  mapStateToProps,
+  { fetchNewsRequested }
 )(HomeScreen);
