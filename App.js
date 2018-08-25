624942e6766cde31bad2b492b0bd84298a2dd788
Arkalyk Akash <arkalyk.akash@nu.edu.kz>
2018-08-25 15:54:15 +0200

    removed some of the notification configs

--- App.js
+++ App.js
@@ -1,30 +1,28 @@
-import React, { Component } from 'react';
-import { Platform, UIManager } from 'react-native';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import React, { Component } from "react";
+import { Platform, UIManager } from "react-native";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import { store, persistor } from './src/store';
-import { DrawerNavigator } from './src/components/navigation';
-import { setupFCM } from './src/config';
+import { store, persistor } from "./src/store";
+import { DrawerNavigator } from "./src/components/navigation";
 
 export default class App extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		setupFCM();
+    if (Platform.OS === "android") {
+      UIManager.setLayoutAnimationEnabledExperimental &&
+        UIManager.setLayoutAnimationEnabledExperimental(true);
+    }
+  }
 
-		if (Platform.OS === 'android') {
-			UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
-		}
-	}
-
-	render() {
-		return (
-			<Provider store={store}>
-				<PersistGate persistor={persistor}>
-					<DrawerNavigator />
-				</PersistGate>
-			</Provider>
-		);
-	}
+  render() {
+    return (
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <DrawerNavigator />
+        </PersistGate>
+      </Provider>
+    );
+  }
 }
--- android/app/src/main/java/com/coaktau/MainApplication.java
+++ android/app/src/main/java/com/coaktau/MainApplication.java
@@ -3,7 +3,6 @@ package com.coaktau;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.evollu.react.fcm.FIRMessagingPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,7 +24,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new FIRMessagingPackage(),
             new MapsPackage()
       );
     }
