792a70582aba4edae8d21ac837dacac305baf0c1
aatalyk <atalyk.akash@nu.edu.kz>
2018-08-26 05:12:20 -0700

    Small NewsPage changes

--- src/components/news/NewsPage.js
+++ src/components/news/NewsPage.js
@@ -5,47 +5,48 @@ import PropTypes from 'prop-types';
 import { textStyles } from '../../assets';
 
 const propTypes = {
-  navigation: PropTypes.object,
+	navigation: PropTypes.object
 };
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class NewsPage extends Component {
-  render() {
-    const item = this.props.navigation.getParam('item');
-    const lang = this.props.navigation.getParam('lang');
+	render() {
+		const item = this.props.navigation.getParam('item');
+		const lang = this.props.navigation.getParam('lang');
 
-    return (
-      <ScrollView style={styles.container}>
-        <Image source={{ uri: item.imageUrl }} style={styles.image} />
-        <Text style={styles.title}>{item[lang].title}</Text>
-        <Text style={styles.description}>{item[lang].content}</Text>
-      </ScrollView>
-    );
-  }
+		return (
+			<ScrollView style={styles.container}>
+				<Image source={{ uri: item.imageUrl }} style={styles.image} />
+				<Text style={styles.title}>{item[lang].title}</Text>
+				<Text style={styles.description}>{item[lang].content}</Text>
+			</ScrollView>
+		);
+	}
 }
 
 NewsPage.propTypes = propTypes;
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'column',
-    backgroundColor: 'white',
-  },
-  image: {
-    width: SCREEN_WIDTH,
-    height: 200,
-  },
-  title: {
-    marginHorizontal: 15,
-    marginTop: 10,
-    ...textStyles.h1,
-  },
-  description: {
-    marginHorizontal: 15,
-    marginTop: 10,
-    ...textStyles.p,
-  },
+	container: {
+		flexDirection: 'column',
+		backgroundColor: 'white'
+	},
+	image: {
+		width: SCREEN_WIDTH,
+		height: 200
+	},
+	title: {
+		marginHorizontal: 15,
+		marginTop: 10,
+		...textStyles.h1
+	},
+	description: {
+		marginHorizontal: 15,
+		marginTop: 10,
+		marginBottom: 20,
+		...textStyles.p
+	}
 });
 
 export { NewsPage };
