12645eff2cad908d32fc3a794d5106308df6e60f
Arkalyk Akash <arkalyk.akash@nu.edu.kz>
2018-08-31 22:16:05 +0200

    Problems with pod files removed

--- ios/COAktau/AppDelegate.m
+++ ios/COAktau/AppDelegate.m
@@ -18,7 +18,8 @@
 {
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  // DEBUG: jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"COAktau"
