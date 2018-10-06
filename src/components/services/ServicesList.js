2fe6bc5b7dbdef8a0e045f1a46441fe73fa0f7ef
Arkalyk Akash <arkalyk.akash@nu.edu.kz>
2018-10-06 15:15:56 +0200

    services fetched from backend

--- src/components/services/ServicesList.js
+++ src/components/services/ServicesList.js
@@ -1,135 +1,155 @@
-import React, { Component } from 'react';
-import { View, FlatList, StyleSheet, TouchableOpacity, Text, RefreshControl } from 'react-native';
-import { StackActions } from 'react-navigation';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import {
+  View,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  RefreshControl
+} from "react-native";
+import { StackActions } from "react-navigation";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-import { ServiceItem } from './ServiceItem';
-import { MessageScreen } from '../common';
-import { fetchServicesRequested } from '../../actions';
-import { colors, textStyles, settings, images } from '../../assets';
+import { ServiceItem } from "./ServiceItem";
+import { MessageScreen } from "../common";
+import { fetchServicesRequested } from "../../actions";
+import { colors, textStyles, settings, images } from "../../assets";
 
 const propTypes = {
-	style: PropTypes.object,
-	navigation: PropTypes.object,
-	isPartiallyShown: PropTypes.bool,
-	lang: PropTypes.string,
-	loading: PropTypes.bool,
-	data: PropTypes.array,
-	fetchServicesRequested: PropTypes.func,
-	myServices: PropTypes.array,
-	showsMyServices: PropTypes.bool
+  style: PropTypes.object,
+  navigation: PropTypes.object,
+  isPartiallyShown: PropTypes.bool,
+  lang: PropTypes.string,
+  loading: PropTypes.bool,
+  data: PropTypes.array,
+  fetchServicesRequested: PropTypes.func,
+  myServices: PropTypes.array,
+  showsMyServices: PropTypes.bool
 };
 
 class ServicesListScreen extends Component {
-	componentDidMount() {
-		this.props.fetchServicesRequested();
-		const data = this.test();
-		console.log(data);
-	}
+  componentDidMount() {
+    this.props.fetchServicesRequested();
+  }
 
-	test = () => {
-		fetch('https://facebook.github.io/react-native/movies.json')
-			.then(response => response.json())
-			.then(responseJson => {
-				return responseJson;
-			})
-			.catch(error => {
-				console.log(error);
-			});
-	};
+  getShortData = data => data.slice(0, 2);
 
-	getShortData = data => data.slice(0, 2);
+  onRefresh = () => this.props.fetchServicesRequested();
 
-	onRefresh = () => this.props.fetchServicesRequested();
+  renderItem = ({ item }) => (
+    <ServiceItem item={item} onPress={() => this.onPress(item)} />
+  );
 
-	renderItem = ({ item }) => <ServiceItem item={item} onPress={() => this.onPress(item)} />;
+  keyExtractor = (_, index) => index + "";
 
-	keyExtractor = (_, index) => index + '';
+  renderSeparator = () => <View style={styles.separator} />;
 
-	renderSeparator = () => <View style={styles.separator} />;
+  onShowMorePress = () => {
+    const action = StackActions.push({
+      routeName: "ServicesList"
+    });
+    this.props.navigation.dispatch(action);
+  };
 
-	onShowMorePress = () => {
-		const action = StackActions.push({
-			routeName: 'ServicesList'
-		});
-		this.props.navigation.dispatch(action);
-	};
+  onPress = item => this.props.navigation.navigate("Services", { item });
 
-	onPress = item => this.props.navigation.navigate('Services', { item });
+  render() {
+    const {
+      isPartiallyShown,
+      loading,
+      data,
+      lang,
+      showsMyServices,
+      myServices
+    } = this.props;
+    const serviceItems =
+      isPartiallyShown || showsMyServices ? myServices : data;
 
-	render() {
-		const { isPartiallyShown, loading, data, lang, showsMyServices, myServices } = this.props;
-		const serviceItems = isPartiallyShown || showsMyServices ? myServices : data;
-
-		return (
-			<View style={[styles.container, this.props.style, { flex: !isPartiallyShown ? 1 : undefined }]}>
-				<View style={{ flex: 1 }}>
-					{serviceItems.length === 0 ? (
-						<MessageScreen
-							style={styles.noDataText}
-							title={settings[lang].text.noMyServices}
-							imgSource={isPartiallyShown ? null : images.box}
-						/>
-					) : (
-						<FlatList
-							data={serviceItems}
-							renderItem={this.renderItem}
-							keyExtractor={this.keyExtractor}
-							ItemSeparatorComponent={this.renderSeparator}
-							refreshControl={<RefreshControl refreshing={loading} onRefresh={this.onRefresh} />}
-						/>
-					)}
-				</View>
-				{isPartiallyShown && (
-					<TouchableOpacity style={styles.showMoreButton} onPress={this.onShowMorePress}>
-						<Text style={styles.showMoreText}>{settings[lang].navigation.services}</Text>
-					</TouchableOpacity>
-				)}
-			</View>
-		);
-	}
+    return (
+      <View
+        style={[
+          styles.container,
+          this.props.style,
+          { flex: !isPartiallyShown ? 1 : undefined }
+        ]}
+      >
+        <View style={{ flex: 1 }}>
+          {serviceItems.length === 0 ? (
+            <MessageScreen
+              style={styles.noDataText}
+              title={settings[lang].text.noMyServices}
+              imgSource={isPartiallyShown ? null : images.box}
+            />
+          ) : (
+            <FlatList
+              data={serviceItems}
+              renderItem={this.renderItem}
+              keyExtractor={this.keyExtractor}
+              ItemSeparatorComponent={this.renderSeparator}
+              refreshControl={
+                <RefreshControl
+                  refreshing={loading}
+                  onRefresh={this.onRefresh}
+                />
+              }
+            />
+          )}
+        </View>
+        {isPartiallyShown && (
+          <TouchableOpacity
+            style={styles.showMoreButton}
+            onPress={this.onShowMorePress}
+          >
+            <Text style={styles.showMoreText}>
+              {settings[lang].navigation.services}
+            </Text>
+          </TouchableOpacity>
+        )}
+      </View>
+    );
+  }
 }
 
 ServicesListScreen.propTypes = propTypes;
 
 const styles = StyleSheet.create({
-	container: {
-		backgroundColor: 'white'
-	},
-	separator: {
-		height: 0.5,
-		backgroundColor: colors.grayUltraLight,
-		marginLeft: 10,
-		marginRight: 10
-	},
-	showMoreButton: {
-		flexDirection: 'row',
-		justifyContent: 'flex-end',
-		alignItems: 'center',
-		height: 44
-	},
-	showMoreText: {
-		...textStyles.p,
-		marginRight: 15,
-		color: colors.blue
-	},
-	title: {
-		...textStyles.p,
-		marginLeft: 15,
-		marginTop: 20,
-		color: colors.grayLight
-	}
+  container: {
+    backgroundColor: "white"
+  },
+  separator: {
+    height: 0.5,
+    backgroundColor: colors.grayUltraLight,
+    marginLeft: 10,
+    marginRight: 10
+  },
+  showMoreButton: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    height: 44
+  },
+  showMoreText: {
+    ...textStyles.p,
+    marginRight: 15,
+    color: colors.blue
+  },
+  title: {
+    ...textStyles.p,
+    marginLeft: 15,
+    marginTop: 20,
+    color: colors.grayLight
+  }
 });
 
 const mapStateToProps = ({ services, settings, myServices }) => ({
-	myServices,
-	data: services.data,
-	loading: services.loading,
-	lang: settings.lang
+  myServices,
+  data: services.data,
+  loading: services.loading,
+  lang: settings.lang
 });
 
 export const ServicesList = connect(
-	mapStateToProps,
-	{ fetchServicesRequested }
+  mapStateToProps,
+  { fetchServicesRequested }
 )(ServicesListScreen);
--- src/logic/servicesLogic.js
+++ src/logic/servicesLogic.js
@@ -1,19 +1,26 @@
-import { createLogic } from 'redux-logic';
+import { createLogic } from "redux-logic";
 
-import { FETCH_SERVICES_REQUESTED, fetchServicesSucceded, fetchServicesFailed } from '../actions';
-import { fetch } from '../config';
+import {
+  FETCH_SERVICES_REQUESTED,
+  fetchServicesSucceded,
+  fetchServicesFailed
+} from "../actions";
 
 const fetchServicesLogic = createLogic({
-	type: FETCH_SERVICES_REQUESTED,
-	process: async (_, dispatch, done) => {
-		try {
-			const items = await fetch('services');
-			dispatch(fetchServicesSucceded(items));
-		} catch (error) {
-			dispatch(fetchServicesFailed(error));
-		}
-		done();
-	}
+  type: FETCH_SERVICES_REQUESTED,
+  process: ({ getState }, dispatch, done) => {
+    const { lang } = getState().settings;
+    fetch("http://soaktau.kz/api/v1.00/services")
+      .then(response => response.json())
+      .then(json => {
+        const services = json[lang];
+        dispatch(fetchServicesSucceded(services));
+      })
+      .catch(error => {
+        dispatch(fetchServicesFailed(`${error}`));
+      })
+      .then(() => done());
+  }
 });
 
 export const servicesLogic = [fetchServicesLogic];
