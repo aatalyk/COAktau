c7382beeaddf436b02c3571b8bc6a1d3998e2b87
Arkalyk Akash <arkalyk.akash@nu.edu.kz>
2018-10-13 21:25:59 +0200

    passing renderItem as props to AndroidViewPager

--- src/components/home/AndroidPagingView.js
+++ src/components/home/AndroidPagingView.js
@@ -1,51 +1,43 @@
-import React, { Component } from 'react';
-import { View, ViewPagerAndroid, StyleSheet, Image, TouchableWithoutFeedback, Dimensions } from 'react-native';
-import PropTypes from 'prop-types';
-
-import { images } from '../../assets';
+import React, { Component } from "react";
+import { View, ViewPagerAndroid, StyleSheet } from "react-native";
+import PropTypes from "prop-types";
 
 const propTypes = {
-	data: PropTypes.arrayOf(PropTypes.shape({ imageUrl: PropTypes.string.isRequired })),
-	style: PropTypes.oneOf([PropTypes.object, PropTypes.number, PropTypes.array]),
-	onPageSelected: PropTypes.func,
-	onItemPress: PropTypes.func
+  data: PropTypes.arrayOf(
+    PropTypes.shape({ imageUrl: PropTypes.string.isRequired })
+  ),
+  style: PropTypes.oneOf([PropTypes.object, PropTypes.number, PropTypes.array]),
+  onPageSelected: PropTypes.func,
+  renderItem: PropTypes.func
 };
 
-const SCREEN_WIDTH = Dimensions.get('window').width;
-
 class AndroidPagingView extends Component {
-	onPageSelected = ({ nativeEvent }) => {
-		this.props.onPageSelected(nativeEvent.position);
-	};
+  onPageSelected = ({ nativeEvent }) => {
+    this.props.onPageSelected(nativeEvent.position);
+  };
 
-	renderItem = (item, index) => (
-    <View key={`${index}`}>
-		<TouchableWithoutFeedback onPress={this.props.onItemPress} >
-			<Image
-				defaultSource={images.imgPlaceholder}
-				resizeMode="cover"
-				style={{ width: SCREEN_WIDTH, height: 150 }}
-				source={{ uri: item.imageUrl || undefined }}
-			/>
-		</TouchableWithoutFeedback>
-    </View>
-	);
+  renderItem = (item, index) => (
+    <View key={`${index}`}>{this.props.renderItem({ item })}</View>
+  );
 
-	render() {
-		return (
-			<ViewPagerAndroid style={[styles.viewPager, this.props.style]} onPageSelected={this.onPageSelected}>
-				{this.props.data.map((item, index) => this.renderItem(item, index))}
-			</ViewPagerAndroid>
-		);
-	}
+  render() {
+    return (
+      <ViewPagerAndroid
+        style={[styles.viewPager, this.props.style]}
+        onPageSelected={this.onPageSelected}
+      >
+        {this.props.data.map((item, index) => this.renderItem(item, index))}
+      </ViewPagerAndroid>
+    );
+  }
 }
 
 AndroidPagingView.propTypes = propTypes;
 
 const styles = StyleSheet.create({
-	viewPager: {
-		flex: 1
-	}
+  viewPager: {
+    flex: 1
+  }
 });
 
 export { AndroidPagingView };
