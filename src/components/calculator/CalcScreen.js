b1772df86cd8c4979e2323791537ee82b2f2cf38
Arkalyk Akash <arkalyk.akash@nu.edu.kz>
2018-10-15 10:50:25 +0200

    KeyboardAvoidingView added to calculator screen

--- src/components/calculator/CalcScreen.js
+++ src/components/calculator/CalcScreen.js
@@ -1,65 +1,87 @@
-import React, { Component } from 'react';
-import { ScrollView, FlatList, StyleSheet } from 'react-native';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import {
+  ScrollView,
+  FlatList,
+  StyleSheet,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform
+} from "react-native";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-import { Calculator } from './Calculator';
-import { FAQItem } from './FAQItem';
-import { fetchCalcParamsRequested, fetchCalcFaqRequested } from '../../actions';
+import { Calculator } from "./Calculator";
+import { FAQItem } from "./FAQItem";
+import { fetchCalcParamsRequested, fetchCalcFaqRequested } from "../../actions";
 
 const propTypes = {
-	navigation: PropTypes.object,
-	lang: PropTypes.string,
-	fetchCalcParamsRequested: PropTypes.func,
-	fetchCalcFaqRequested: PropTypes.func,
-	params: PropTypes.object,
-	faq: PropTypes.object
+  navigation: PropTypes.object,
+  lang: PropTypes.string,
+  fetchCalcParamsRequested: PropTypes.func,
+  fetchCalcFaqRequested: PropTypes.func,
+  params: PropTypes.object,
+  faq: PropTypes.object
 };
 
 class Calc extends Component {
-	componentDidMount() {
-		this.fetchParamsFAQ();
-	}
+  componentDidMount() {
+    this.fetchParamsFAQ();
+  }
 
-	renderItem = ({ item, index }) => <FAQItem item={item} index={index} />;
+  renderItem = ({ item, index }) => <FAQItem item={item} index={index} />;
 
-	fetchParamsFAQ = () => {
-		this.props.fetchCalcParamsRequested();
-		this.props.fetchCalcFaqRequested();
-	};
+  fetchParamsFAQ = () => {
+    this.props.fetchCalcParamsRequested();
+    this.props.fetchCalcFaqRequested();
+  };
 
-	render() {
-		const { faq, params, navigation, lang } = this.props;
-		return (
-			<ScrollView style={styles.container}>
-				<Calculator
-					lang={lang}
-					navigation={navigation}
-					livingCost={params.livingCost}
-					povertyMin={params.povertyMin}
-				/>
-				<FlatList data={faq} renderItem={this.renderItem} keyExtractor={(_, index) => index + ''} />
-			</ScrollView>
-		);
-	}
+  onScroll() {
+    Keyboard.dismiss();
+  }
+
+  render() {
+    const { faq, params, navigation, lang } = this.props;
+    return (
+      <ScrollView style={styles.container} onScroll={this.onScroll}>
+        <KeyboardAvoidingView
+          behavior={Platform.select({
+            ios: "position",
+            android: null
+          })}
+        >
+          <Calculator
+            lang={lang}
+            navigation={navigation}
+            livingCost={params.livingCost}
+            povertyMin={params.povertyMin}
+          />
+          <FlatList
+            data={faq}
+            renderItem={this.renderItem}
+            keyExtractor={(_, index) => index + ""}
+          />
+        </KeyboardAvoidingView>
+      </ScrollView>
+    );
+  }
 }
 
 Calc.propTypes = propTypes;
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: 'white'
-	}
+  container: {
+    flex: 1,
+    backgroundColor: "white"
+  }
 });
 
 const mapStateToProps = ({ settings, calc }) => ({
-	lang: settings.lang,
-	params: calc.params,
-	faq: calc.faq
+  lang: settings.lang,
+  params: calc.params,
+  faq: calc.faq
 });
 
 export const CalcScreen = connect(
-	mapStateToProps,
-	{ fetchCalcParamsRequested, fetchCalcFaqRequested }
+  mapStateToProps,
+  { fetchCalcParamsRequested, fetchCalcFaqRequested }
 )(Calc);
--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -1,158 +1,174 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-	View,
-	Text,
-	TextInput,
-	Button,
-	StyleSheet,
-	KeyboardAvoidingView,
-	Alert,
-	Keyboard,
-	TouchableWithoutFeedback,
-	Dimensions
-} from 'react-native';
-import PropTypes from 'prop-types';
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Alert,
+  Keyboard,
+  TouchableWithoutFeedback,
+  Dimensions,
+  Platform
+} from "react-native";
+import PropTypes from "prop-types";
 
-import { colors, textStyles, settings } from '../../assets';
-import { calculate } from './actions';
+import { colors, textStyles, settings } from "../../assets";
+import { calculate } from "./actions";
 
 const propTypes = {
-	livingCost: PropTypes.number,
-	povertyMin: PropTypes.number,
-	navigation: PropTypes.object,
-	lang: PropTypes.string
+  livingCost: PropTypes.number,
+  povertyMin: PropTypes.number,
+  navigation: PropTypes.object,
+  lang: PropTypes.string
 };
 
 class Calculator extends Component {
-	state = {
-		amount: 0,
-		numberOfPeople: 0
-	};
+  state = {
+    amount: 0,
+    numberOfPeople: 0
+  };
 
-	onPress = () => {
-		if (this.state.amount === 0 || this.state.numberOfPeople === 0) {
-			this.showAlert();
-			return;
-		}
-		const result = calculate(
-			this.state.amount,
-			this.state.numberOfPeople,
-			this.props.povertyMin,
-			this.props.livingCost
-		);
+  onPress = () => {
+    if (this.state.amount === 0 || this.state.numberOfPeople === 0) {
+      this.showAlert();
+      return;
+    }
+    const result = calculate(
+      this.state.amount,
+      this.state.numberOfPeople,
+      this.props.povertyMin,
+      this.props.livingCost
+    );
 
-		this.props.navigation.navigate('AlertScreen', { result });
-	};
+    this.props.navigation.navigate("AlertScreen", { result });
+  };
 
-	changeAmount = amount => {
-		if (amount === '') {
-			this.setState((prevState, prevProps) => ({
-				...prevState,
-				amount: 0
-			}));
-		} else {
-			this.setState((prevState, prevProps) => ({
-				...prevState,
-				amount
-			}));
-		}
-	};
+  changeAmount = amount => {
+    if (amount === "") {
+      this.setState((prevState, prevProps) => ({
+        ...prevState,
+        amount: 0
+      }));
+    } else {
+      this.setState((prevState, prevProps) => ({
+        ...prevState,
+        amount
+      }));
+    }
+  };
 
-	changeNumberOfPeople = numberOfPeople => {
-		if (numberOfPeople === '') {
-			this.setState((prevState, prevProps) => ({
-				...prevState,
-				numberOfPeople: 0
-			}));
-		} else {
-			this.setState((prevState, prevProps) => ({
-				...prevState,
-				numberOfPeople
-			}));
-		}
-	};
+  changeNumberOfPeople = numberOfPeople => {
+    if (numberOfPeople === "") {
+      this.setState((prevState, prevProps) => ({
+        ...prevState,
+        numberOfPeople: 0
+      }));
+    } else {
+      this.setState((prevState, prevProps) => ({
+        ...prevState,
+        numberOfPeople
+      }));
+    }
+  };
 
-	showAlert = () => {
-		const { lang } = this.props;
-		Alert.alert(
-			settings[this.props.lang].text.warning,
-			'',
-			[{ text: settings[lang].text.cancel, onPress: () => console.log('Cancel Pressed'), style: 'destructive' }],
-			{ cancelable: true }
-		);
-	};
+  showAlert = () => {
+    const { lang } = this.props;
+    Alert.alert(
+      settings[this.props.lang].text.warning,
+      "",
+      [
+        {
+          text: settings[lang].text.cancel,
+          onPress: () => console.log("Cancel Pressed"),
+          style: "destructive"
+        }
+      ],
+      { cancelable: true }
+    );
+  };
 
-	render() {
-		const { lang } = this.props;
-		return (
-			<TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-				<View style={styles.container}>
-					<Text style={styles.title}>{settings[lang].calc.title}</Text>
-					<Text style={styles.text}>{settings[lang].calc.text1}</Text>
-					<TextInput
-						style={styles.textInput}
-						keyboardType="number-pad"
-						onChangeText={value => this.changeAmount(value)}
-						placeholder={settings[lang].text.input}
-					/>
-					<Text style={styles.text}>{settings[this.props.lang].calc.text2}</Text>
-					<TextInput
-						style={styles.textInput}
-						keyboardType="number-pad"
-						onChangeText={value => this.changeNumberOfPeople(value)}
-						placeholder={settings[lang].text.input}
-					/>
-					<View style={styles.button}>
-						<Button title={settings[lang].buttons.calculate} color="white" onPress={this.onPress} />
-					</View>
-					<Text style={styles.warning}>{this.state.warning}</Text>
-				</View>
-			</TouchableWithoutFeedback>
-		);
-	}
+  render() {
+    const { lang } = this.props;
+    return (
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <View style={styles.container}>
+          <Text style={styles.title}>{settings[lang].calc.title}</Text>
+          <Text style={styles.text}>{settings[lang].calc.text1}</Text>
+          <TextInput
+            style={styles.textInput}
+            keyboardType="number-pad"
+            onChangeText={value => this.changeAmount(value)}
+            placeholder={settings[lang].text.input}
+          />
+
+          <Text style={styles.text}>
+            {settings[this.props.lang].calc.text2}
+          </Text>
+
+          <TextInput
+            style={styles.textInput}
+            keyboardType="number-pad"
+            onChangeText={value => this.changeNumberOfPeople(value)}
+            placeholder={settings[lang].text.input}
+          />
+
+          <View style={styles.button}>
+            <Button
+              title={settings[lang].buttons.calculate}
+              color="white"
+              onPress={this.onPress}
+            />
+          </View>
+          <Text style={styles.warning}>{this.state.warning}</Text>
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  }
 }
 
 Calculator.propTypes = propTypes;
 
 const styles = StyleSheet.create({
-	container: {
-		margin: 10,
-		justifyContent: 'center',
-		alignItems: 'center'
-	},
-	title: {
-		...textStyles.h1,
-		textAlign: 'center',
-		fontSize: 20,
-		color: colors.purple,
-		margin: 10
-	},
-	text: {
-		...textStyles.h1,
-		textAlign: 'center',
-		margin: 10
-	},
-	warning: {
-		...textStyles.p,
-		fontSize: 12,
-		color: 'red',
-		marginLeft: 20,
-		marginRight: 20
-	},
-	textInput: {
-		backgroundColor: colors.grayUltraLight,
-		height: 44,
-		width: Dimensions.get('window').width * 0.7,
-		margin: 10,
-		padding: 10,
-		...textStyles.p
-	},
-	button: {
-		height: 44,
-		width: Dimensions.get('window').width * 0.7,
-		backgroundColor: colors.purple,
-		margin: 10
-	}
+  container: {
+    margin: 10,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  title: {
+    ...textStyles.h1,
+    textAlign: "center",
+    fontSize: 20,
+    color: colors.purple,
+    margin: 10
+  },
+  text: {
+    ...textStyles.h1,
+    textAlign: "center",
+    margin: 10
+  },
+  warning: {
+    ...textStyles.p,
+    fontSize: 12,
+    color: "red",
+    marginLeft: 20,
+    marginRight: 20
+  },
+  textInput: {
+    backgroundColor: colors.grayUltraLight,
+    height: 44,
+    width: Dimensions.get("window").width * 0.7,
+    margin: 10,
+    padding: 10,
+    ...textStyles.p
+  },
+  button: {
+    height: 44,
+    width: Dimensions.get("window").width * 0.7,
+    backgroundColor: colors.purple,
+    margin: 10
+  }
 });
 
 export { Calculator };
