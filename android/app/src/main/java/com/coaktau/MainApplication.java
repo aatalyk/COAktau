78397eb0bed8c0c6822475b76ce8dbe5b88de889
aatalyk <atalyk.akash@nu.edu.kz>
2018-10-18 16:24:09 +0600

    fix text input

--- android/app/src/main/java/com/coaktau/MainApplication.java
+++ android/app/src/main/java/com/coaktau/MainApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.evollu.react.fcm.FIRMessagingPackage;
 import com.facebook.react.ReactApplication;
+import com.nikolaiwarner.RNTextInputReset.RNTextInputResetPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -36,6 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNTextInputResetPackage(),
             new ReactVideoPackage(),
             new MapsPackage(),
             new LinearGradientPackage(),
