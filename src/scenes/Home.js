39401ff19f895429bc5b352241286aab91525e99
aatalyk <atalyk.akash@nu.edu.kz>
2018-10-21 14:12:01 +0600

    Add animted scrollY

--- src/scenes/Home.js
+++ src/scenes/Home.js
@@ -36,7 +36,7 @@ class HomeScreen extends Component {
 			}
 		],
 		connected: true,
-		scrollY: new Animated.Value(Platform.OS === 'ios' ? -getStatusBarHeight() : 0)
+		scrollY: new Animated.Value(Platform.OS === 'ios' ? -250 : 0)
 	};
 
 	constructor(props) {
@@ -72,19 +72,46 @@ class HomeScreen extends Component {
 
 	onTabViewIndexChange = index => this.setState({ index });
 
+	onScroll = () => {
+		console.warn('onScroll');
+		Animated.event([{ nativeEvent: { contentOffset: { y: this.state.scrollY } } }], {
+			useNativeDriver: true
+		});
+	};
+
 	render() {
 		const { lang, navigation, newsItems } = this.props;
+
 		const news = newsItems.filter(newsItem => newsItem.isMain);
+
+		const scrollY = Animated.add(this.state.scrollY, Platform.OS === 'ios' ? 0 : 0);
+
+		const headerTranslate = scrollY.interpolate({
+			inputRange: [0, 250],
+			outputRange: [0, -250],
+			extrapolate: 'clamp'
+		});
+
+		console.warn(headerTranslate);
+
 		return (
-			<View style={styles.container}>
-				<Animated.View>
-					<AutoPagingFlatList data={news} lang={lang} navigation={navigation} />
-				</Animated.View>
+			<Animated.View style={[styles.container, { transform: [{ translateY: headerTranslate }] }]}>
+				<AutoPagingFlatList data={news} lang={lang} navigation={navigation} />
 				<TabView
 					renderTabBar={this.renderTabBar}
 					navigationState={this.state}
 					renderScene={SceneMap({
-						first: () => <Notifications navigation={navigation} />,
+						first: () => (
+							<Notifications
+								onScroll={Animated.event(
+									[{ nativeEvent: { contentOffset: { y: this.state.scrollY } } }],
+									{
+										useNativeDriver: true
+									}
+								)}
+								navigation={navigation}
+							/>
+						),
 						second: () => <ServicesAndFAQ navigation={navigation} />
 					})}
 					onIndexChange={this.onTabViewIndexChange}
@@ -92,7 +119,7 @@ class HomeScreen extends Component {
 					style={styles.tabView}
 					useNativeDriver
 				/>
-			</View>
+			</Animated.View>
 		);
 	}
 }
