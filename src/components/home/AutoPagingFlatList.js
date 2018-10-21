cee5b12a79da867c63cb784e8ea4322e8e31931e
Arkalyk Akash <arkalyk.akash@nu.edu.kz>
2018-10-21 22:41:44 +0200

    added gradient background to news item text

--- src/components/home/AutoPagingFlatList.js
+++ src/components/home/AutoPagingFlatList.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
-import { View, Text, FlatList, StyleSheet, Image, Dimensions, Platform, TouchableWithoutFeedback } from 'react-native';
-import PageControl from 'react-native-page-control';
-import PropTypes from 'prop-types';
+import React, {Component} from "react";
+import {View, Text, FlatList, StyleSheet, Image, Dimensions, Platform, TouchableWithoutFeedback} from "react-native";
+import LinearGradient from "react-native-linear-gradient";
+import PageControl from "react-native-page-control";
+import PropTypes from "prop-types";
 
-import { images, colors, textStyles } from '../../assets';
-import { AndroidPagingView } from './AndroidPagingView';
+import {images, colors, textStyles} from "../../assets";
+import {AndroidPagingView} from "./AndroidPagingView";
 
 const propTypes = {
 	data: PropTypes.array,
@@ -13,29 +14,29 @@ const propTypes = {
 	manualPaging: PropTypes.bool,
 	onItemPress: PropTypes.func,
 	loadFinish: PropTypes.func,
-	style: PropTypes.oneOf([PropTypes.object, PropTypes.number])
+	style: PropTypes.oneOf([PropTypes.object, PropTypes.number]),
 };
 
-const SCREEN_WIDTH = Dimensions.get('window').width;
+const SCREEN_WIDTH = Dimensions.get("window").width;
 const HEIGHT = 200;
 
 class AutoPagingFlatList extends Component {
 	state = {
-		currentIndex: 0
+		currentIndex: 0,
 	};
 
 	componentDidMount() {
-		if (Platform.OS === 'ios' && !this.props.manualPaging) {
+		if (Platform.OS === "ios" && !this.props.manualPaging) {
 			this.interval = setInterval(() => {
-				const { currentIndex } = this.state;
-				const { data } = this.props;
+				const {currentIndex} = this.state;
+				const {data} = this.props;
 				const count = data.length;
 				const nextIndex = count > 1 && currentIndex < count - 1 ? currentIndex + 1 : 0;
 
 				if (count > 0) {
 					this.flatList.scrollToIndex({
 						index: nextIndex,
-						animated: true
+						animated: true,
 					});
 				}
 			}, 2500);
@@ -43,7 +44,7 @@ class AutoPagingFlatList extends Component {
 	}
 
 	componentWillUnmount() {
-		if (Platform.OS === 'ios') {
+		if (Platform.OS === "ios") {
 			clearInterval(this.interval);
 		}
 	}
@@ -53,48 +54,55 @@ class AutoPagingFlatList extends Component {
 		const viewSize = event.nativeEvent.layoutMeasurement;
 
 		const currentIndex = Math.floor(contentOffset.x / viewSize.width);
-		this.setState({ currentIndex });
+		this.setState({currentIndex});
 	};
 
 	getItemLayout = (data, index) => ({
 		length: SCREEN_WIDTH,
 		offset: SCREEN_WIDTH * index,
-		index
+		index,
 	});
 
 	onItemPress = () =>
 		this.props.navigation &&
-		this.props.navigation.navigate('NewsPage', {
+		this.props.navigation.navigate("NewsPage", {
 			item: this.props.data[this.state.currentIndex],
-			lang: this.props.lang
+			lang: this.props.lang,
 		});
 
-	renderItem = ({ item }) => (
+	renderItem = ({item}) => (
 		<TouchableWithoutFeedback onPress={this.props.onItemPress || this.onItemPress}>
 			<View style={styles.imageContainer}>
-				<Image
-					defaultSource={images.imgPlaceholder}
-					style={styles.image}
-					source={{ uri: item.icon || undefined }}
-					onLoad={this.props.loadFinish}
-				/>
-				{item.title && (
-					<Text style={styles.itemText} numberOfLines={2}>
-						{item.title}
-					</Text>
-				)}
+				<View style={styles.roundedImageContainer}>
+					<Image
+						defaultSource={images.imgPlaceholder}
+						style={styles.image}
+						source={{uri: item.icon || undefined}}
+						onLoad={this.props.loadFinish}
+					/>
+					{!!item.title && (
+						<LinearGradient
+							colors={["rgba(0,0,0,0)", "rgba(0,0,0,0.5)", "rgba(0,0,0,1)"]}
+							style={styles.gradientTextWrapper}
+						>
+							<Text style={styles.itemText} numberOfLines={2}>
+								{item.title}
+							</Text>
+						</LinearGradient>
+					)}
+				</View>
 			</View>
 		</TouchableWithoutFeedback>
 	);
 
-	keyExtractor = (_, index) => index + '';
+	keyExtractor = (_, index) => index + "";
 
 	render() {
-		const { data, style } = this.props;
+		const {data, style} = this.props;
 
 		return (
 			<View style={[styles.container, style]}>
-				{Platform.OS === 'ios' ? (
+				{Platform.OS === "ios" ? (
 					<FlatList
 						ref={ref => {
 							this.flatList = ref;
@@ -102,7 +110,7 @@ class AutoPagingFlatList extends Component {
 						data={data}
 						renderItem={this.renderItem}
 						horizontal
-						pagingEnabled={Platform.OS === 'ios'}
+						pagingEnabled={Platform.OS === "ios"}
 						onMomentumScrollEnd={this.onScrollEnd}
 						getItemLayout={this.getItemLayout}
 						showsHorizontalScrollIndicator={false}
@@ -112,7 +120,7 @@ class AutoPagingFlatList extends Component {
 					<AndroidPagingView
 						data={data}
 						renderItem={this.renderItem}
-						onPageSelected={position => this.setState({ currentIndex: position })}
+						onPageSelected={position => this.setState({currentIndex: position})}
 					/>
 				)}
 				{!!data && (
@@ -137,44 +145,56 @@ AutoPagingFlatList.propTypes = propTypes;
 const styles = StyleSheet.create({
 	container: {
 		marginTop: 10,
-		overflow: 'visible',
+		overflow: "visible",
 		height: 250,
-		width: SCREEN_WIDTH
+		width: SCREEN_WIDTH,
 	},
 	imageContainer: {
 		width: SCREEN_WIDTH,
-		height: HEIGHT
+		height: HEIGHT,
+		overflow: "hidden",
 	},
 	view: {},
 	image: {
 		width: SCREEN_WIDTH * 0.9,
 		height: HEIGHT,
+	},
+	roundedImageContainer: {
+		width: SCREEN_WIDTH * 0.9,
+		height: HEIGHT,
 		borderRadius: 10,
-		alignSelf: 'center',
-		marginHorizontal: SCREEN_WIDTH * 0.05
+		alignSelf: "center",
+		marginHorizontal: SCREEN_WIDTH * 0.05,
+		overflow: "hidden",
 	},
 	pageControl: {
-		position: 'absolute',
+		position: "absolute",
 		bottom: 10,
-		alignSelf: 'center',
+		alignSelf: "center",
 		backgroundColor: colors.soLightBlue,
 		paddingVertical: 7,
-		borderRadius: 7
+		borderRadius: 7,
 	},
 	pageControlIndicator: {
-		borderRadius: 5
+		borderRadius: 5,
 	},
 	pageControlIndicatorSize: {
 		width: 8,
-		height: 8
+		height: 8,
 	},
 	itemText: {
 		...textStyles.h3,
-		position: 'absolute',
+		marginBottom: 15,
 		marginHorizontal: SCREEN_WIDTH * 0.05 + 10,
-		bottom: 10,
-		backgroundColor: 'rgba(0, 0, 0, 0.2)'
-	}
+	},
+	gradientTextWrapper: {
+		height: HEIGHT / 2,
+		justifyContent: "flex-end",
+		position: "absolute",
+		bottom: 0,
+		left: 0,
+		right: 0,
+	},
 });
 
-export { AutoPagingFlatList };
+export {AutoPagingFlatList};
